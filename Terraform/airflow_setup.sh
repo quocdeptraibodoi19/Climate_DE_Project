83b0fdb2134b1fb08aa6d155531c6bbc6c639a6e
quocdeptraibodoi19 <quocthogminhqtm@gmail.com>
2023-02-20 20:33:22 +0700

    Complete the docker-compose file to set up the airflow

--- Terraform/airflow_setup.sh
+++ Terraform/airflow_setup.sh
@@ -15,9 +15,10 @@ sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plu
 sudo chmod 666 /var/run/docker.sock
 echo 'Clone git repo to EC2'
 cd /home/ubuntu && git clone "https://github.com/quocdeptraibodoi19/Climate_DE_Project.git" && cd Climate_DE_Project
-sudo mkdir -p logs plugins temp dags tests migrations 
+sudo mkdir -p logs dags
 mv ./ETL_Process/Airflow_Custom_Operators.py ./dags
 mv ./ETL_Process/airflow_S3_load_Script.py ./dags
 mv ./ETL_Process/Spark_integrate_script.py ./dags
 mv ./ETL_Process/Spark_process_script.py ./dags
+docker-compose up
 
