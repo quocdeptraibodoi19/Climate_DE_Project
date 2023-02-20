8308e7f492820df1a34ae417b02c3955b5276670
quocdeptraibodoi19 <quocthogminhqtm@gmail.com>
2023-02-20 21:24:34 +0700

    Fix the airflow_setup.sh to download the docker to the cloud instance

--- Terraform/airflow_setup.sh
+++ Terraform/airflow_setup.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
 echo "-------------------------START SETUP---------------------------"
-echo "Setting up Docker..."
+sudo apt-get -y update
+
 sudo apt-get -y install \
 ca-certificates \
 curl \
 gnupg \
-lsb-release \
-unzip
+lsb-release
+
+sudo apt -y install unzip
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
 sudo apt-get -y update
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 sudo chmod 666 /var/run/docker.sock
--- Terraform/output.tf
+++ Terraform/output.tf
@@ -1,12 +1,12 @@
 output "private_key" {
   description = "EC2 private key."
-  value       = tls_private_key.custom_key.private_key_pem
+  value       = tls_private_key.custom-key.private_key_pem
   sensitive   = true
 }
 
 output "public_key" {
   description = "EC2 public key."
-  value       = tls_private_key.custom_key.public_key_openssh
+  value       = tls_private_key.custom-key.public_key_openssh
 }
 
 output "airflow_public_dns" {
