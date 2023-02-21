0288802306bbcc4247388b785096d3dfd39148bc
quocdeptraibodoi19 <quocthogminhqtm@gmail.com>
2023-02-21 08:45:37 +0700

    Fixing the docker-compose file

--- Terraform/airflow_setup.sh
+++ Terraform/airflow_setup.sh
@@ -8,10 +8,12 @@ cd /home/ubuntu && git clone "https://github.com/quocdeptraibodoi19/Climate_DE_P
 sudo chmod 777 ./Climate_DE_Project
 cd Climate_DE_Project
 sudo mkdir -p logs dags
+sudo chmod 777 ./logs
+sudo chmod 777 ./dags
 mv ./ETL_Process/Airflow_Custom_Operators.py ./dags
 mv ./ETL_Process/airflow_S3_load_Script.py ./dags
 mv ./ETL_Process/Spark_integrate_script.py ./dags
 mv ./ETL_Process/Spark_process_script.py ./dags
 sudo apt-get -y install docker-compose
-docker-compose up
+sudo docker-compose up
 
