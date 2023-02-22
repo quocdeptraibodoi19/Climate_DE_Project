6ef64a9b162ce0e1ef1ac46626b8857000678c32
quocdeptraibodoi19 <quocthogminhqtm@gmail.com>
2023-02-22 13:23:31 +0700

    tune the script docker

--- Terraform/airflow_setup.sh
+++ Terraform/airflow_setup.sh
@@ -29,5 +29,6 @@ mv ./ETL_Process/Airflow_Custom_Operators.py ./dags
 mv ./ETL_Process/airflow_S3_load_Script.py ./dags
 mv ./ETL_Process/Spark_integrate_script.py ./dags
 mv ./ETL_Process/Spark_process_script.py ./dags
+sudo apt install -y docker-compose
 sudo docker-compose up
 
